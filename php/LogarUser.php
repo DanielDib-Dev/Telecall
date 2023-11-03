ffe0d7bd1370b90a77534540cef209019941071c
Daniel Dib <dan-dib@hotmail.com>
2023-11-03 20:39:51 -0300

    Back-end

--- php/LogarUser.php
+++ php/LogarUser.php
@@ -2,13 +2,15 @@
 require 'Conexao.php';
 
 session_start(); // Inicie a sessão no início do script
+$_SESSION['Logado'] = FALSE;
+$_SESSION['Usuario'] = 'Área do cliente';
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     $Usuario = $_POST['Usuario'];
     $Senha = $_POST['Senha'];
     $Perfil = $_POST['Perfil'];
 
-    $verifica = mysql_query("SELECT * FROM usuarios WHERE Usuario = '$Usuario' AND Senha = '$Senha'") or die("erro ao selecionar");
+    $verifica = mysql_query("SELECT * FROM usuarios WHERE Usuario = '$Usuario' AND Senha = '$Senha' AND Perfil = '$Perfil'") or die("erro ao selecionar");
 
     if (mysql_num_rows($verifica)<=0){
         echo"<script language='javascript' type='text/javascript'>
@@ -19,6 +21,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         // Armazenar informações do usuário na sessão
         $_SESSION['Usuario'] = $Usuario;
         $_SESSION['Perfil'] = $Perfil;
+        $_SESSION['Logado'] = TRUE;
         header("Location: ../index.html");
     }
 
